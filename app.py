efc31089f3f5ed41556ff13464c1cda2eea4a16e
PC\Wojtek <W.P.Marczuk@student.tudelft.nl>
2022-05-13 02:00:32 +0200

    GUI upgrade

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import sys
 def convert():
     try:
         sp.preview('\Huge $' + latex_entry.get() + '$', viewer='file', filename='images/res.png')
-        res = ImageTk.PhotoImage(Image.open("images/res.png"))
+        res = ImageTk.PhotoImage(Image.open('images/res.png'))
         image.configure(text='')
         image.configure(image=res)
         image.image = res
@@ -21,23 +21,40 @@ def convert():
 root = tk.Tk()
 root.option_add('*font', 'Lato')
 root.title('Latex Whiteboard')
-root.geometry('700x400')
+root.geometry('900x400')
+icon = ImageTk.PhotoImage(Image.open('images/x_icon.png'))
+root.iconphoto(False, icon)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=3)
+
+# Insert logo
+logoframe = tk.Frame(root, bg='white')
+logoframe.grid(column=0, row=0, sticky=tk.N)
+logo = ImageTk.PhotoImage(Image.open('images/latex_whiteboard.png'))
+logo_label = tk.Label(logoframe, image=logo, borderwidth=0)
+logo_label.image = logo
+logo_label.grid(column=0, row=0, sticky=tk.N, pady=100)
 
 # Label for latex entry
-latex_label = tk.Label(root, text='Enter latex string:')
-latex_label.pack()
+enter_latex_string_image = ImageTk.PhotoImage(Image.open('images/enter_latex_expression.png'))
+latex_label = tk.Label(root, image=enter_latex_string_image, width=350)
+latex_label.image = enter_latex_string_image
+latex_label.grid(column=1, row=0, sticky=tk.N, pady=5)
 
 # Latex entry
 latex_entry = tk.Entry(root, width=70)
-latex_entry.pack()
+latex_entry.grid(column=1, row=0, sticky=tk.N, pady=40)
 
 # Convert button
-convert_button = tk.Button(root, text='Convert', command=convert)
-convert_button.pack(pady=10)
+convert_image = ImageTk.PhotoImage(Image.open('images/convert.png'))
+convert_button = tk.Button(root, image=convert_image, command=convert, borderwidth=0.5)
+convert_button.image = convert_image
+convert_button.grid(column=1, row=0, sticky=tk.N, pady=80)
 
 # Resulting expression image
-res = None
-image = tk.Label(root, image=res)
-image.pack(pady=10)
+result_frame = tk.Frame(root)
+result_frame.grid(column=1, row=0, sticky=tk.N, pady=150)
+image = tk.Label(result_frame)
+image.pack()
 
 root.mainloop()
