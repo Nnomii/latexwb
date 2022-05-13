883942e730f47cc6de41ff802edde8f959dd2c9c
PC\Wojtek <W.P.Marczuk@student.tudelft.nl>
2022-05-13 02:38:55 +0200

    Threading to display a "generating" message during image creation

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import sympy as sp
 import sys
+from threading import Thread
 
 
-# Convert command (converts latex input and updates the resulting expression image)
-def convert():
+# Updates the image field to inform the user that the expression image is generating
+def generate(event=None):
+    image.configure(image='')
+    image.configure(text='Generating...')
+
+
+# Make image command (transforms latex input into a resulting expression image)
+def make_image(event=None):
     try:
         sp.preview('\Huge $' + latex_entry.get() + '$', viewer='file', filename='images/res.png')
         res = ImageTk.PhotoImage(Image.open('images/res.png'))
@@ -13,8 +20,17 @@ def convert():
         image.configure(image=res)
         image.image = res
     except Exception as e:
+        # An exception at this point means that there is a syntax error in the user's latex input
         image.configure(image='')
-        image.configure(text=str(e).split(')')[-1].replace(r'\r\n', '\n'))
+        image.configure(text=str(e).split(')')[-1].replace(r'\r\n', '\n')) # Displays the latex error
+
+
+# Command tied to the convert button to start the image creation process
+def convert(event=None):
+    t1 = Thread(target=generate)
+    t2 = Thread(target=make_image)
+    t1.start()
+    t2.start()
 
 
 # Initialise window
@@ -22,8 +38,7 @@ root = tk.Tk()
 root.option_add('*font', 'Lato')
 root.title('Latex Whiteboard')
 root.geometry('900x400')
-icon = ImageTk.PhotoImage(Image.open('images/x_icon.png'))
-root.iconphoto(False, icon)
+root.iconphoto(False, ImageTk.PhotoImage(Image.open('images/x_icon.png')))
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=3)
 
@@ -50,6 +65,7 @@ convert_image = ImageTk.PhotoImage(Image.open('images/convert.png'))
 convert_button = tk.Button(root, image=convert_image, command=convert, borderwidth=0.5)
 convert_button.image = convert_image
 convert_button.grid(column=1, row=0, sticky=tk.N, pady=80)
+root.bind('<Return>', convert)
 
 # Resulting expression image
 result_frame = tk.Frame(root)
